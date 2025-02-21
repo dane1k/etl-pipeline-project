8e70a6ebab59c58c0f6138b914649dc178616622
dane1k <danekdb.nz@gmail.com>
2025-02-21 14:16:46 +1300

    Created python script to find a data about s&p500 and send it to PostgresSQL

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-print("hello world!")
-nums = 2 + 2
+import yfinance as yf
+import pandas as pd
+from sqlalchemy import create_engine
 
-print(nums)
+db_url = "postgresql://postgres:danekNZsukableat2004-@localhost:5432/stocks_db"
+engine = create_engine(db_url)
+
+try:
+    with engine.connect() as connection:
+        print("Connected!")
+except Exception as e:
+    print("Didnt connect TnT :", e)
+
+
+stock = yf.Ticker("AAPL")
+data = stock.history(period="1mo")
+data.reset_index(inplace=True)
+data.rename(columns={"Date": "date"}, inplace=True)
+
+data.to_sql("stocks_data", engine, if_exists="replace", index=False)
+print("Data has been transfered to stocks_data")
+
+df = pd.read_sql("SELECT * FROM stocks_data", engine)
+print(df.head())
