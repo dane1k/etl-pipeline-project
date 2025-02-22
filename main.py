5411dfb27ef740e3efc1300a31bafd3763caf321
dane1k <danekdb.nz@gmail.com>
2025-02-23 12:32:41 +1300

    Updated script to find out many shareses prices and info about them

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ try:
 except Exception as e:
     print("Didnt connect TnT :", e)
 
+symbols = ['BTC-USD', '^GSPC', 'ETH-USD']
+for symbol in symbols:
+    stock = yf.Ticker(symbol)
+    data = stock.history(period="1mo")
+    data.reset_index(inplace=True)
+    data.rename(columns={"Date": "date"}, inplace=True)
+    data['symbol'] = symbol
+    data['name'] = stock.info.get('longName')
 
-stock = yf.Ticker("AAPL")
-data = stock.history(period="1mo")
-data.reset_index(inplace=True)
-data.rename(columns={"Date": "date"}, inplace=True)
-
-data.to_sql("stocks_data", engine, if_exists="replace", index=False)
-print("Data has been transfered to stocks_data")
+    data.to_sql("stocks_data", engine, if_exists="append", index=False)
+    print(f"Data for {symbol} has been added to stocks_data")
 
 # testing
 df = pd.read_sql("SELECT * FROM stocks_data", engine)
-print(df.head())
+print(f"\n \n \n \n {df.head()}")
