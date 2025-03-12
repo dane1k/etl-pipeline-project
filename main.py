6ae5d228e8504abe400def579f3d6a0edb7fab98
dane1k <danekdb.nz@gmail.com>
2025-03-12 18:09:08 +1300

    Updating data

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-import yfinance as yf
 import pandas as pd
-from sqlalchemy import create_engine
 
-db_url = "postgresql://postgres:danekNZsukableat2004-@localhost:5432/stocks_db"
-engine = create_engine(db_url)
+data = { 'Name' : ['Danya', 'Vishwa', 'Sanya', 'Filip'],
+        'Age': [20, 21, 18, 22],
+        'City': ['Auckland', 'Auckland', 'Auckland', 'Auckland']}
 
-try:
-    with engine.connect() as connection:
-        print("Connected!")
-except Exception as e:
-    print("Didnt connect TnT :", e)
+df = pd.DataFrame(data)
 
-symbols = ['BTC-USD', '^GSPC', 'ETH-USD']
-for symbol in symbols:
-    stock = yf.Ticker(symbol)
-    data = stock.history(period="1mo")
-    data.reset_index(inplace=True)
-    data.rename(columns={"Date": "date"}, inplace=True)
-    data['symbol'] = symbol
-    data['name'] = stock.info.get('longName')
-
-    data.to_sql("stocks_data", engine, if_exists="append", index=False)
-    print(f"Data for {symbol} has been added to stocks_data")
-
-# testing
-df = pd.read_sql("SELECT * FROM stocks_data", engine)
-print(f"\n \n \n \n {df.head()}")
+print(df)
